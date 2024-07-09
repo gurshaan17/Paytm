96cbff3ec651c8567fa4f7790341a79ab8306e07
gurshaan17 <gurshaansinghd@gmail.com>
2024-07-09 19:27:23 +0530

    prisma init

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
+
+
 export default function Home() {
   return (
     <div className="text-4xl">
